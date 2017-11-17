6740be54cdb5b1b330b52bb0cb49b6c4dd9ef61d
Really-Dream <Really_Dream@126.com>
2017-11-17 08:51:34 +0800

    post

--- src/main/java/com/imooc/sell/service/impl/OrderServiceImpl.java
+++ src/main/java/com/imooc/sell/service/impl/OrderServiceImpl.java
@@ -1,13 +1,21 @@
 package com.imooc.sell.service.impl;
 
 import com.imooc.sell.dataobject.OrderDetail;
+import com.imooc.sell.dataobject.OrderMaster;
 import com.imooc.sell.dataobject.ProductInfo;
+import com.imooc.sell.dto.CartDTO;
 import com.imooc.sell.dto.OrderDTO;
+import com.imooc.sell.enums.OrderStatusEnum;
+import com.imooc.sell.enums.PayStatusEnum;
 import com.imooc.sell.enums.ResultEnum;
 import com.imooc.sell.exception.SellException;
+import com.imooc.sell.repository.OrderDetailRepository;
+import com.imooc.sell.repository.OrderMasterRepository;
 import com.imooc.sell.service.OrderService;
 import com.imooc.sell.service.ProductService;
+import com.imooc.sell.utils.KeyUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +23,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Dream
@@ -28,9 +38,16 @@ public class OrderServiceImpl implements OrderService{
     @Autowired
     ProductService productService;
 
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    OrderMasterRepository orderMasterRepository;
+
     @Override
     public OrderDTO create(OrderDTO orderDTO) {
 
+        String orderId = KeyUtil.genUniqueKey();
         BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
 
         //1.查询商品（数量，价格）
@@ -41,13 +58,31 @@ public class OrderServiceImpl implements OrderService{
             }
 
             //2.计算订单总价
+            orderAmount = orderDetail.getProductPrice()
+                    .multiply(new BigDecimal(orderDetail.getProductQuantity()))
+                    .add(orderAmount);
 
             //3.订单详情入库
+            orderDetail.setDetailId(KeyUtil.genUniqueKey());
+            orderDetail.setOrderId(orderId);
+            BeanUtils.copyProperties(productInfo,orderDetail);
+            orderDetailRepository.save(orderDetail);
         }
 
         //4.写入订单数据库（orderMaster和orderDetail）
+        OrderMaster orderMaster = new OrderMaster();
+        orderDTO.setOrderId(orderId);
+        BeanUtils.copyProperties(orderDTO,orderMaster);
+        orderMaster.setOrderAmount(orderAmount);
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMasterRepository.save(orderMaster);
 
         //5.扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream()
+                .map(e -> new CartDTO(e.getProductId(),e.getProductQuantity()))
+                .collect(Collectors.toList());
+        productService.decreaseStock(cartDTOList);
 
         //发送websocket消息
         return null;
